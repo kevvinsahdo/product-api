872bfc2e0615f17f6e1f963ff97a04034ea94c21
Kevvin Sahdo <kevvinsd@gmail.com>
2020-06-14 16:27:11 -0400

    fix: Fix url field type

--- src/main/java/com/productapi/domain/Product.java
+++ src/main/java/com/productapi/domain/Product.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Product implements Serializable {
@@ -15,6 +16,7 @@ public class Product implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	@Lob
 	private String imageUrl;
 	private Double value;
 	
